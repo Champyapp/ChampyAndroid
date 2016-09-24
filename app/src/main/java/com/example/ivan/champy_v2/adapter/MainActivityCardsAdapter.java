15e4dd996ffd9dc060c678c69319362adcf1766f
SashaKhyzhun <sasha6427@gmail.com>
2016-09-24 03:11:03 +0300

    tempView. cards. meah. 2

--- app/src/main/java/com/example/ivan/champy_v2/adapter/MainActivityCardsAdapter.java
+++ app/src/main/java/com/example/ivan/champy_v2/adapter/MainActivityCardsAdapter.java
@@ -27,7 +27,6 @@ import com.example.ivan.champy_v2.model.SelfImprovement_model;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static java.lang.Math.round;
 
 public class MainActivityCardsAdapter extends CustomPagerAdapter /*implements View.OnClickListener*/ {
 
@@ -35,10 +34,6 @@ public class MainActivityCardsAdapter extends CustomPagerAdapter /*implements Vi
     public static final String TAG = "CardsAdapterMain";
     private String token, userId;
     private Snackbar snackbar;
-    private Button buttonDoneForToday, buttonGiveUp, buttonShare;
-    private ChallengeController cc;
-    private SelfImprovement_model currentCard;
-    private String itemWakeUpTime;
 
     public MainActivityCardsAdapter(Context context, ArrayList<SelfImprovement_model> marrayList) {
         super(context);
@@ -52,9 +47,9 @@ public class MainActivityCardsAdapter extends CustomPagerAdapter /*implements Vi
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             tempView = inflater.inflate(R.layout.single_card_fragment_self, null, false);
         }
-        final SelfImprovement_model item = arrayList.get(position);
+        final SelfImprovement_model currentCard = arrayList.get(position);
         ImageView cardImage = (ImageView)tempView.findViewById(R.id.cardImage);
-
+        Typeface typeface = android.graphics.Typeface.createFromAsset(getContext().getAssets(), "fonts/bebasneue.ttf");
         WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         Point size = new Point();
@@ -67,102 +62,85 @@ public class MainActivityCardsAdapter extends CustomPagerAdapter /*implements Vi
         cardImage.getLayoutParams().height = y*50;
         if (y > 10) y = 10;
 
-        final ChallengeController challengeController = new ChallengeController(getContext(), (Activity) getContext(), 0 , 0, 0);
 
         TextView tvChallengeType = (TextView) tempView.findViewById(R.id.tvChallengeType);
-        tvChallengeType.setText(item.getType());
-        String itemGoal = item.getGoal();
-        ImageView imageView = (ImageView)tempView.findViewById(R.id.imageViewChallengeLogo);
+        tvChallengeType.setText(currentCard.getType());
+        tvChallengeType.setTextSize((float)(y*1.3));
+        tvChallengeType.setTypeface(typeface);
 
-        switch (item.getType()) {
+        String itemGoal = currentCard.getGoal();
+        ImageView imageChallengeLogo = (ImageView)tempView.findViewById(R.id.imageViewChallengeLogo);
+
+        switch (currentCard.getType()) {
             case "Wake Up":
-                imageView.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.wakeup_white));
-                itemGoal = item.getChallengeName();
+                imageChallengeLogo.setImageResource(R.drawable.wakeup_white);
+                itemGoal = currentCard.getChallengeName();
                 break;
             case "Duel":
-                itemGoal = item.getGoal();
-                imageView.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.duel_white));
+//                itemGoal = item.getGoal();
+                imageChallengeLogo.setImageResource(R.drawable.duel_white);
                 break;
             case "Self-Improvement":
-                imageView.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.self_white));
+                imageChallengeLogo.setImageResource(R.drawable.self_white);
                 break;
         }
 
-        tvChallengeType.setTextSize((float)(y*1.3));
-        Typeface typeface = android.graphics.Typeface.createFromAsset(getContext().getAssets(), "fonts/bebasneue.ttf");
-        tvChallengeType.setTypeface(typeface);
+
 
         TextView tvChallengeDescription = (TextView) tempView.findViewById(R.id.tvChallengeDescription);
         tvChallengeDescription.setText(itemGoal);
-        tvChallengeDescription.setTextSize(y);
+        tvChallengeDescription.setTextSize(y*2);
+        tvChallengeDescription.setTypeface(typeface);
 
         TextView tvDuration = (TextView) tempView.findViewById(R.id.textViewDuration);
-        tvDuration.setText(item.getDays() + " DAYS TO GO");
+        tvDuration.setText(currentCard.getDays() + " DAYS TO GO");
         tvDuration.setTextSize(y*2);
 
-        Button buttonGiveUp = (Button) tempView.findViewById(R.id.buttonGiveUp);
-        buttonGiveUp.getLayoutParams().width = x*10;
+        final Button buttonGiveUp = (Button) tempView.findViewById(R.id.buttonGiveUp);
+        buttonGiveUp.getLayoutParams().width  = x*10;
         buttonGiveUp.getLayoutParams().height = x*10;
+        final Button buttonDone = (Button) tempView.findViewById(R.id.buttonDoneForToday);
+        buttonDone.getLayoutParams()  .width  = x*10;
+        buttonDone.getLayoutParams()  .height = x*10;
+        final Button buttonShare = (Button) tempView.findViewById(R.id.buttonShare);
+        buttonShare.getLayoutParams() .width  = x*10;
+        buttonShare.getLayoutParams() .height = x*10;
+
+
+        if (currentCard.getUpdated() != null){
+            if (!currentCard.getType().equals("Wake Up")) { //?
+                if (currentCard.getUpdated().equals("false")) {
+                    buttonShare.setVisibility(View.INVISIBLE);
+                    buttonDone.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
 
         CurrentUserHelper user = new CurrentUserHelper(getContext());
         token = user.getToken();
         userId = user.getUserObjectId();
 
+        final ChallengeController challengeController = new ChallengeController(getContext(), (Activity) getContext(), 0 , 0, 0);
+
         buttonGiveUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which){
-                            case DialogInterface.BUTTON_POSITIVE:
-                                try {
-                                    if (item.getType().equals("Wake Up")) {
-                                        int intentId = Integer.parseInt(item.getGoal());
-                                        challengeController.give_up(item.getId(), intentId, token, userId);
-                                    } else {
-                                        challengeController.give_up(item.getId(), 0, token, userId);
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                                break;
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                break;
-                        }
-                    }
-                };
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle(R.string.areYouSure)
-                        .setMessage(R.string.youWantToGiveUp)
-                        .setIcon(R.drawable.ic_action_warn)
-                        .setCancelable(false)
-                        .setPositiveButton(R.string.yes, dialogClickListener)
-                        .setNegativeButton(R.string.no, dialogClickListener).show();
-
+                try {
+                    if (currentCard.getType().equals("Wake Up")) {
+                           int intentId = Integer.parseInt(currentCard.getGoal());
+                           challengeController.give_up(currentCard.getId(), intentId, token, userId);
+                    } else challengeController.give_up(currentCard.getId(), 0, token, userId);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
-
-        final Button buttonDoneForToday = (Button) tempView.findViewById(R.id.buttonDoneForToday);
-        buttonDoneForToday.getLayoutParams().width = x*10;
-        buttonDoneForToday.getLayoutParams().height = x*10;
-
-        //final Button finalButton = buttonDoneForToday;
-        if (item.getUpdated() != null){
-            if (!item.getType().equals("Wake Up")) {
-                if (item.getUpdated().equals("false")) {
-                    buttonDoneForToday.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.icon_done_for_today));
-//                        tvDoneForToday.setVisibility(View.VISIBLE);
-                }
-            }
-        }
-
-        buttonDoneForToday.setOnClickListener(new View.OnClickListener() {
+        buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id = item.getId();
+                String id = currentCard.getId();
                 SQLiteDatabase localSQLiteDatabase = new DBHelper(getContext()).getWritableDatabase();
                 ContentValues localContentValues = new ContentValues();
                 localContentValues.put("updated", "true");
@@ -170,10 +148,22 @@ public class MainActivityCardsAdapter extends CustomPagerAdapter /*implements Vi
                 int i = localSQLiteDatabase.update("updated", localContentValues, "challenge_id = ?", new String[]{id});
                 try {
                     challengeController.doneForToday(id, token, userId);
+                    buttonDone.setVisibility(View.INVISIBLE);
+                    buttonShare.setVisibility(View.VISIBLE);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                buttonDoneForToday.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.icon_share));
+            }
+        });
+
+        buttonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "I'm done for today with my challenge: " + currentCard.getGoal() + "\nTry and you - www.champyapp.com/download";
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_TEXT, message);
+                getContext().startActivity(Intent.createChooser(share, "How would you like to share?"));
             }
         });
 
