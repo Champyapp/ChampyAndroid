d29276b8b016493ce2629225dbdd87fc76fdbb2d
SashaKhyzhun <sasha.khyzhun@gmail.com>
2016-11-08 14:19:55 +0200

    changed itemCard and improved RoleControllerActivity

--- app/src/main/java/com/example/ivan/champy_v2/activity/RoleControllerActivity.java
+++ app/src/main/java/com/example/ivan/champy_v2/activity/RoleControllerActivity.java
@@ -1,78 +1,108 @@
 package com.example.ivan.champy_v2.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.ivan.champy_v2.utils.ChallengeController;
 import com.example.ivan.champy_v2.R;
-import com.example.ivan.champy_v2.utils.SessionManager;
 import com.example.ivan.champy_v2.helper.CurrentUserHelper;
 import com.example.ivan.champy_v2.helper.NotificationController;
-import com.github.nkzawa.emitter.Emitter;
-import com.github.nkzawa.socketio.client.Socket;
+import com.example.ivan.champy_v2.utils.ChallengeController;
+import com.example.ivan.champy_v2.utils.OfflineMode;
+import com.example.ivan.champy_v2.utils.SessionManager;
 
-public class RoleControllerActivity extends AppCompatActivity {
+public class RoleControllerActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "RoleControllerActivity";
-    private Socket mSocket;
-    //public View spinner;
-
-    private Emitter.Listener onConnect = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            CurrentUserHelper currentUser = new CurrentUserHelper(getApplicationContext());
-            mSocket.emit("ready", currentUser.getToken());
-            Log.i(TAG, "Sockets: onConnect");
-        }
-    };
-    private Emitter.Listener onConnected = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            Log.i(TAG, "Sockets: onConnected");
-        }
-    };
+    private SessionManager sessionManager;
+    private OfflineMode offlineMode;
+    private TextView lostInternet;
+    private ImageView imageReload;
+//    private Socket mSocket;
+//    //public View spinner;
+//
+//    private Emitter.Listener onConnect = new Emitter.Listener() {
+//        @Override
+//        public void call(final Object... args) {
+//            CurrentUserHelper currentUser = new CurrentUserHelper(getApplicationContext());
+//            mSocket.emit("ready", currentUser.getToken());
+//            Log.i(TAG, "Sockets: onConnect");
+//        }
+//    };
+//    private Emitter.Listener onConnected = new Emitter.Listener() {
+//        @Override
+//        public void call(final Object... args) {
+//            Log.i(TAG, "Sockets: onConnected");
+//        }
+//    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_role_controller);
+
+        Typeface typeface = android.graphics.Typeface.createFromAsset(getAssets(), "fonts/bebasneue.ttf");
+        TextView tvChampy = (TextView)findViewById(R.id.tvChampy);
+        tvChampy.setTypeface(typeface);
+
 //        spinner = findViewById(R.id.loadingPanel);
 
 //        try {
 //            mSocket = IO.socket("http://46.101.213.24:3007");
 //        } catch (URISyntaxException e) { throw new RuntimeException(e); }
 
-        SessionManager sessionManager = new SessionManager(getApplicationContext());
+        sessionManager = new SessionManager(getApplicationContext());
+        offlineMode = new OfflineMode();
 
-
-        if (sessionManager.isUserLoggedIn()) {
-//            mSocket.on("connect", onConnect);
-//            mSocket.on("connected", onConnected);
-//            mSocket.connect();
-
-            CurrentUserHelper user = new CurrentUserHelper(getApplicationContext());
-            String uId = user.getUserObjectId();
-            String uToken = user.getToken();
-            ChallengeController cc = new ChallengeController(getApplicationContext(), this, uToken, uId);
-            cc.generateCardsForMainActivity();
+        if (offlineMode.isConnectedToRemoteAPI(this)) {
+            checkIfLoggedInAndMakeRedirect();
         } else {
-//            mSocket.off();
-//            mSocket.disconnect();
-            NotificationController controller = new NotificationController(getApplicationContext());
-            controller.deactivateDailyNotificationReminder();
-            Intent goToActivity = new Intent(this, LoginActivity.class);
-            startActivity(goToActivity);
+            lostInternet = (TextView)findViewById(R.id.tvLostInternetConnection);
+            imageReload = (ImageView)findViewById(R.id.imageRetry);
+            lostInternet.setTypeface(typeface);
+
+            lostInternet.setVisibility(View.VISIBLE);
+            imageReload.setVisibility(View.VISIBLE);
+            imageReload.setOnClickListener(this);
         }
+
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (offlineMode.isConnectedToRemoteAPI(this)) {
+            lostInternet.setVisibility(View.INVISIBLE);
+            imageReload.setVisibility(View.INVISIBLE);
+            checkIfLoggedInAndMakeRedirect();
+        }
+    }
 
     @Override
     protected void onStop() {
         super.onStop();
         //spinner.setVisibility(View.INVISIBLE);
     }
+
+
+    private void checkIfLoggedInAndMakeRedirect() {
+        if (sessionManager.isUserLoggedIn()) {
+            CurrentUserHelper user = new CurrentUserHelper(getApplicationContext());
+            String uId = user.getUserObjectId();
+            String uToken = user.getToken();
+            ChallengeController cc = new ChallengeController(getApplicationContext(), this, uToken, uId);
+            cc.generateCardsForMainActivity();
+        } else {
+            NotificationController controller = new NotificationController(getApplicationContext());
+            controller.deactivateDailyNotificationReminder();
+            Intent goToActivity = new Intent(this, LoginActivity.class);
+            startActivity(goToActivity);
+        }
+    }
+
 }
