28a7dd4ba5e6a66607e6707c829c705457864721
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-01-13 14:27:18 +0200

    test for mainActivity

---
+++ app/src/test/java/com/azinecllc/champy/activity/MainActivityTest.java
@@ -0,0 +1,69 @@
+package com.azinecllc.champy.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.widget.ImageView;
+
+import com.azinecllc.champy.BuildConfig;
+import com.azinecllc.champy.R;
+import com.azinecllc.champy.helper.CHDownloadImageTask;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SashaKhyzhun on 1/13/17.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivityTest {
+
+    private Activity mainActivity;
+    private ImageView background;
+    @Mock
+    Context context;
+    @Mock
+    CHDownloadImageTask imageTask;
+
+
+    @Before
+    public void setup() throws Exception {
+        mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+        imageTask = Mockito.mock(CHDownloadImageTask.class);
+        context   = Mockito.mock(Context.class);
+        imageTask = new CHDownloadImageTask(context, (MainActivity)mainActivity);
+
+    }
+
+    @Test
+    public void testActivity() throws Exception {
+        assertNotNull(mainActivity);
+        assertEquals("MainActivity", mainActivity.getLocalClassName());
+    }
+
+    @Test
+    public void testBackgroundImageView() throws Exception {
+        background = (ImageView)mainActivity.findViewById(R.id.main_background);
+        assertNotNull(background);
+    }
+
+//    @Test
+//    public void onCreate() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void onClick() throws Exception {
+//
+//    }
+
+}
--- app/src/test/java/com/azinecllc/champy/controller/ChallengeControllerTest.java
+++ app/src/test/java/com/azinecllc/champy/controller/ChallengeControllerTest.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import com.azinecllc.champy.activity.MainActivity;
 import com.azinecllc.champy.data.DBHelper;
@@ -20,10 +19,11 @@ import org.mockito.Mock;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Random;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by SashaKhyzhun on 12/27/16.
@@ -32,53 +32,49 @@ public class ChallengeControllerTest {
     @Mock
     Context mockContext;
     @Mock
-    DBHelper dbHelper;
+    DBHelper mockDBHelper;
     @Mock
-    Cursor c;
+    Cursor mockCursor;
     @Mock
-    SQLiteDatabase db;
-    @Mock
-    AlarmManager aManager;
-    @Mock
-    Intent mockIntent;
-    @Mock
-    PendingIntent mockPI;
+    SQLiteDatabase mockDB;
     @Mock
     Activity mockActivity;
 
 
     @Before
     public void setup() throws Exception {
-        mockContext = mock(Context.class);
-        dbHelper = DBHelper.getInstance(mockContext);
-        c = mock(Cursor.class);
-        db = mock(SQLiteDatabase.class);
-        aManager = mock(AlarmManager.class);
-        mockIntent = mock(Intent.class);
-        mockPI = mock(PendingIntent.class);
+        mockContext  = mock(Context.class);
+        mockDBHelper = mock(DBHelper.class);
+        mockCursor   = mock(Cursor.class);
+        mockDB       = mock(SQLiteDatabase.class);
         mockActivity = mock(MainActivity.class);
 
     }
 
     @Test
-    public void setNewAlarmClock() throws Exception {
+    public void testMethodForSetNewAlarmManager() throws Exception {
 
         Calendar c = GregorianCalendar.getInstance();
         int days  = c.get(Calendar.DAY_OF_YEAR);
         int hour  = c.get(Calendar.HOUR_OF_DAY);
         int min   = c.get(Calendar.MINUTE);
 
-        Assert.assertNotNull(c);
-        Assert.assertNotNull(days);
-        Assert.assertNotNull(hour);
-        Assert.assertNotNull(min);
+        System.out.println("days: " + days);
+        System.out.println("hour: " + hour);
+        System.out.println("min: " + min);
+
+        assertNotNull(c);
+        assertNotNull(days);
+        assertNotNull(hour);
+        assertNotNull(min);
 
         final long currentMidnight = System.currentTimeMillis() / 1000
                 - (c.get(Calendar.HOUR_OF_DAY) * 60 * 60)
                 - (c.get(Calendar.MINUTE) * 60)
                 - (c.get(Calendar.SECOND));
 
-        Assert.assertNotNull(currentMidnight);
+        System.out.println("currentMidnight: " + currentMidnight);
+        assertNotNull(currentMidnight);
 
         final String[] details = new String[days];
         for (int i = 0; i < days; i++) {
@@ -90,14 +86,23 @@ public class ChallengeControllerTest {
             );
         }
 
-        Assert.assertEquals(notNull(), Arrays.toString(details));
+        if (Arrays.toString(details).equals("[]")) {
+            System.out.println("damn");
+            Assert.assertEquals(String.valueOf(details.length), String.valueOf(1), "[]");
+        } else {
+            assertNotNull(Arrays.toString(details));
+        }
+        System.out.println("Array with time: " + Arrays.toString(details));
 
         for (int i = 0; i <= details.length - 1; i++) {
             if (System.currentTimeMillis() / 1000 < Integer.parseInt(details[i])) {
-                mockPI = PendingIntent.getBroadcast(mockContext, 0, mockIntent, 0);
-                aManager = (AlarmManager)mockContext.getSystemService(Context.ALARM_SERVICE);
-                aManager.set(AlarmManager.RTC_WAKEUP, Long.parseLong(details[i]) * 1000, mockPI);
-                Assert.assertEquals("OK", "OK", "OK");
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Woo-hoo");
+                System.out.println("now < d[i]");
+                System.out.println(System.currentTimeMillis()/1000 + " < " + Integer.parseInt(details[i]));
+                System.out.println("next alarm will be at: " + Long.parseLong(details[i]) * 1000);
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Assert.assertEquals("GOOD", "OK", "OK");
                 break;
             }
 
@@ -107,16 +112,6 @@ public class ChallengeControllerTest {
 
     }
 
-    @Test
-    public void isActive() throws Exception {
-        System.out.println(mockContext);
-        System.out.println(dbHelper);
-        System.out.println(c);
-        System.out.println(db);
-
-        //db = dbHelper.getWritableDatabase();
-
-    }
 
 
 }
