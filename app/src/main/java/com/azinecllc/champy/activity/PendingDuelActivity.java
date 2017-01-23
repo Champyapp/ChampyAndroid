c872e557da3f1164a7bc2027358b49b4a3a37187
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-01-23 12:06:24 +0200

    fixed bug with refreshing pending duels and renamed this files

--- app/src/main/java/com/azinecllc/champy/activity/PendingDuelActivity.java
+++ app/src/main/java/com/azinecllc/champy/activity/PendingDuelActivity.java
@@ -3,6 +3,7 @@ package com.azinecllc.champy.activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -32,85 +33,78 @@ import jp.wasabeef.glide.transformations.CropSquareTransformation;
 
 import static com.azinecllc.champy.utils.Constants.path;
 
-public class PendingDuelActivity extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
+public class PendingDuelActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    //private DrawerLayout drawer;
+    private SessionManager sessionManager;
+    private DrawerLayout drawer;
     public View spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pending__duel);
-        SessionManager sessionManager = SessionManager.getInstance(this);
+        setContentView(R.layout.activity_pending_duel);
+        sessionManager = SessionManager.getInstance(getApplicationContext());
+        new ProgressTask().execute();
 
-        spinner = findViewById(R.id.loadingPanel);
-        spinner.setVisibility(View.VISIBLE);
+        //spinner = findViewById(R.id.loadingPanel);
+        //spinner.setVisibility(View.VISIBLE);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        setSupportActionBar(toolbar);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header_main);
-//        navigationView.setNavigationItemSelectedListener(this);
-//
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        setSupportActionBar(toolbar);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        navigationView.setNavigationItemSelectedListener(this);
+
         Typeface typeface = Typeface.createFromAsset(this.getAssets(), "fonts/bebasneue.ttf");
         TextView tvPendingDuels = (TextView) findViewById(R.id.tvChallengeToMySelf);
         tvPendingDuels.setTypeface(typeface);
 
-//        ImageView drawerImageProfile = (ImageView) headerLayout.findViewById(R.id.profile_image);
-//        ImageView drawerBackground = (ImageView) headerLayout.findViewById(R.id.slide_background);
-//        TextView drawerUserName = (TextView) headerLayout.findViewById(R.id.tvUserName);
-//        drawerBackground.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ImageView drawerImageProfile = (ImageView) headerLayout.findViewById(R.id.profile_image);
+        ImageView drawerBackground = (ImageView) headerLayout.findViewById(R.id.slide_background);
+        TextView drawerUserName = (TextView) headerLayout.findViewById(R.id.tvUserName);
+        drawerBackground.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
-//        File file = new File(path, "profile.jpg");
-//        Uri url = Uri.fromFile(file);
-//
-//        Glide.with(this).load(url).bitmapTransform(new CropCircleTransformation(this))
-//                .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(drawerImageProfile);
+        File file = new File(path, "profile.jpg");
+        Uri url = Uri.fromFile(file);
 
-//        file = new File (path, "blurred.png");
-//        url = Uri.fromFile(file);
-//        Glide.with(this).load(url).bitmapTransform(new CropSquareTransformation(this))
-//                .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(drawerBackground);
+        Glide.with(this).load(url).bitmapTransform(new CropCircleTransformation(this))
+                .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(drawerImageProfile);
 
-        int size = Integer.parseInt(sessionManager.get_duel_pending());
-        PendingDuelsAdapter pagerAdapter = new PendingDuelsAdapter(getSupportFragmentManager());
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_pending_duel);
-        pagerAdapter.setCount(size);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setOffscreenPageLimit(1);
-        viewPager.setPageMargin(20);
-        viewPager.setClipToPadding(false);
-        viewPager.setPadding(90, 0, 90, 0);
+        file = new File(path, "blurred.png");
+        url = Uri.fromFile(file);
+        Glide.with(this).load(url).bitmapTransform(new CropSquareTransformation(this))
+                .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(drawerBackground);
+        String name = sessionManager.getUserName();
+        drawerUserName.setText(name);
+        drawerUserName.setTypeface(typeface);
 
-//        String name = sessionManager.getUserName();
-//        drawerUserName.setText(name);
-//        drawerUserName.setTypeface(typeface);
-
-//        CHCheckPendingDuels checker = CHCheckPendingDuels.getInstance();
-//        int count = checker.getPendingCount(getApplicationContext());
-//        if (count == 0) {
-//            checker.hideItem(navigationView);
-//        } else {
-//            TextView view = (TextView) navigationView.getMenu().findItem(R.id.pending_duels).getActionView();
-//            view.setText(String.format("%s%s", getString(R.string.plus), (count > 0 ? String.valueOf(count) : null)));
-//        }
+        CHCheckPendingDuels checker = CHCheckPendingDuels.getInstance();
+        int count = checker.getPendingCount(getApplicationContext());
+        if (count == 0) {
+            checker.hideItem(navigationView);
+        } else {
+            TextView view = (TextView) navigationView.getMenu().findItem(R.id.pending_duels).getActionView();
+            view.setText(String.format("%s%s", getString(R.string.plus), (count > 0 ? String.valueOf(count) : null)));
+        }
 
         ImageView imageViewLogo = (ImageView) findViewById(R.id.imageViewLogo);
         imageViewLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: 1/20/17 Test code for search bug
-                String uID = sessionManager.getUserId();
-                String token = sessionManager.getToken();
+                SessionManager s = SessionManager.getInstance(getApplicationContext());
+                String uID = s.getUserId();
+                String token = s.getToken();
                 ChallengeController cc = new ChallengeController(getApplicationContext(), PendingDuelActivity.this, token, uID);
                 cc.refreshCardsForPendingDuel(new Intent(getApplicationContext(), PendingDuelActivity.class));
             }
         });
+
         spinner.setVisibility(View.INVISIBLE);
     }
 
@@ -123,58 +117,90 @@ public class PendingDuelActivity extends AppCompatActivity /*implements Navigati
 
     @Override
     public void onBackPressed() {
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-            super.onBackPressed();
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
             Intent intent = new Intent(PendingDuelActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-//        }
+            super.onBackPressed();
+        }
     }
 
+    private class ProgressTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            spinner = findViewById(R.id.loadingPanel);
+            spinner.setVisibility(View.VISIBLE);
+        }
 
-//    @SuppressWarnings("StatementWithEmptyBody")
-//    @Override
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.challenges:
-//                Intent goToChallenges = new Intent(this, MainActivity.class);
-//                startActivity(goToChallenges);
-//                finish();
-//                break;
-//            case R.id.friends:
-//                Intent goToFriends = new Intent(this, FriendsActivity.class);
-//                startActivity(goToFriends);
-//                finish();
-//                break;
-//            case R.id.history:
-//                Intent goToHistory = new Intent(this, HistoryActivity.class);
-//                startActivity(goToHistory);
-//                finish();
-//                break;
-//            case R.id.settings:
-//                Intent goToSettings = new Intent(this, SettingsActivity.class);
-//                startActivity(goToSettings);
-//                finish();
-//                break;
-//            case R.id.share:
-//                String message = getString(R.string.share_text2);
-//                Intent share = new Intent(Intent.ACTION_SEND);
-//                share.setType("text/plain");
-//                share.putExtra(Intent.EXTRA_TEXT, message);
-//                startActivity(Intent.createChooser(share, getString(R.string.how_would_you_like_to_share)));
-//                break;
-//            case R.id.nav_logout:
-//                OfflineMode offlineMode = OfflineMode.getInstance();
-//                if (offlineMode.isConnectedToRemoteAPI(this)) {
-//                    sessionManager.logout(this);
-//                    finish();
-//                }
-//                break;
-//        }
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    SessionManager sessionManager = SessionManager.getInstance(getApplicationContext());
+                    int size = Integer.parseInt(sessionManager.get_duel_pending());
+                    PendingDuelsAdapter pagerAdapter = new PendingDuelsAdapter(getSupportFragmentManager());
+                    ViewPager viewPager = (ViewPager) findViewById(R.id.pager_pending_duel);
+                    pagerAdapter.setCount(size);
+                    viewPager.setAdapter(pagerAdapter);
+                    viewPager.setOffscreenPageLimit(1);
+                    viewPager.setPageMargin(20);
+                    viewPager.setClipToPadding(false);
+                    viewPager.setPadding(90, 0, 90, 0);
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.challenges:
+                Intent goToChallenges = new Intent(this, MainActivity.class);
+                startActivity(goToChallenges);
+                finish();
+                break;
+            case R.id.friends:
+                Intent goToFriends = new Intent(this, FriendsActivity.class);
+                startActivity(goToFriends);
+                finish();
+                break;
+            case R.id.history:
+                Intent goToHistory = new Intent(this, HistoryActivity.class);
+                startActivity(goToHistory);
+                finish();
+                break;
+            case R.id.settings:
+                Intent goToSettings = new Intent(this, SettingsActivity.class);
+                startActivity(goToSettings);
+                finish();
+                break;
+            case R.id.share:
+                String message = getString(R.string.share_text2);
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_TEXT, message);
+                startActivity(Intent.createChooser(share, getString(R.string.how_would_you_like_to_share)));
+                break;
+            case R.id.nav_logout:
+                OfflineMode offlineMode = OfflineMode.getInstance();
+                if (offlineMode.isConnectedToRemoteAPI(this)) {
+                    sessionManager.logout(this);
+                    finish();
+                }
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
