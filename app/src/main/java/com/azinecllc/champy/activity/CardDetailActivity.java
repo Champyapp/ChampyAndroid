046c2badf04a8d5f7567b7b64bae376f26e5ea73
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-03-24 12:54:15 +0200

    working with slider 2

--- app/src/main/java/com/azinecllc/champy/activity/CardDetailActivity.java
+++ app/src/main/java/com/azinecllc/champy/activity/CardDetailActivity.java
@@ -3,6 +3,7 @@ package com.azinecllc.champy.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,6 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.azinecllc.champy.R;
+import com.azinecllc.champy.adapter.CardDetailAdapter;
+import com.azinecllc.champy.model.SectionModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @autor SashaKhyzhun
@@ -35,6 +41,9 @@ public class CardDetailActivity extends AppCompatActivity implements View.OnClic
 
     // Slider Layout
     private RecyclerView recyclerView;
+    private CardDetailAdapter mainAdapter;
+    private List<SectionModel> sections;
+    private List<Integer> items;
 
 
     // Views below slider
@@ -71,6 +80,8 @@ public class CardDetailActivity extends AppCompatActivity implements View.OnClic
 
         // Layout Slider
         recyclerView = (RecyclerView) findViewById(R.id.main_rv);
+        initItems();
+        init();
 
 
         // Views Below slider
@@ -176,6 +187,68 @@ public class CardDetailActivity extends AppCompatActivity implements View.OnClic
         switchReminder.setClickable(true);
         tvChallengeRules.setClickable(true);
     }
+
+    private void initItems() {
+        SectionModel sectionModel1 = new SectionModel();
+        sectionModel1.setLabel("First Section");
+        items = new ArrayList<>();
+        items.add(1);
+        sectionModel1.setItems(items);
+//        items.clear();
+
+        SectionModel sectionModel2 = new SectionModel();
+        sectionModel2.setLabel("Second Section");
+        items = new ArrayList<>();
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        sectionModel2.setItems(items);
+//        items.clear();
+
+        SectionModel sectionModel3 = new SectionModel();
+        sectionModel3.setLabel("Third Section");
+        items = new ArrayList<>();
+        items.add(5);
+        items.add(6);
+        items.add(7);
+        items.add(8);
+        items.add(9);
+        items.add(10);
+        items.add(11);
+        sectionModel3.setItems(items);
+//        items.clear();
+
+        SectionModel sectionModel4 = new SectionModel();
+        sectionModel4.setLabel("Fourth Section");
+        items = new ArrayList<>();
+        items.add(12);
+        items.add(13);
+        items.add(14);
+        items.add(15);
+        items.add(16);
+        items.add(17);
+        items.add(18);
+        items.add(19);
+        items.add(20);
+        items.add(21);
+        sectionModel4.setItems(items);
+//        items.clear();
+
+        sections = new ArrayList<>();
+        sections.add(sectionModel1);
+        sections.add(sectionModel2);
+        sections.add(sectionModel3);
+        sections.add(sectionModel4);
+    }
+
+    private void init() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mainAdapter = new CardDetailAdapter(this, sections);
+        recyclerView.setAdapter(mainAdapter);
+    }
+
 
 
 }
---
+++ app/src/main/java/com/azinecllc/champy/adapter/SectionAdapter.java
@@ -0,0 +1,58 @@
+package com.azinecllc.champy.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.azinecllc.champy.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by SashaKhyzhun on 3/23/17.
+ */
+
+public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Integer> mItems = new ArrayList<>();
+
+    public SectionAdapter(Context context, List<Integer> items) {
+        mContext = context;
+        mItems = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_section, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //holder.itemLabelTextView.setText(String.valueOf(mItems.get(position).toString()));
+        holder.itemLabelTextView.setText('Q');
+        holder.itemLabelTextView.setTextColor(Color.BLACK);
+        holder.itemLabelTextView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView itemLabelTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemLabelTextView = (TextView) itemView.findViewById(R.id.item_label);
+        }
+    }
+}
