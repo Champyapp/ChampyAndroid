b57078503757ae0987d80a363678133e30720067
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-04-04 16:47:43 +0300

    added large text for page 'about'; added new elements in styles; improved about xml;

--- app/src/main/java/com/azinecllc/champy/activity/AboutActivity.java
+++ app/src/main/java/com/azinecllc/champy/activity/AboutActivity.java
@@ -2,12 +2,15 @@ package com.azinecllc.champy.activity;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.azinecllc.champy.R;
 
@@ -17,32 +20,39 @@ public class AboutActivity extends AppCompatActivity {
 
     private WebView webView;
     private View spinner;
+    private TextView tvAbout;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Azinec LLC");
-
-        spinner = findViewById(R.id.loadingPanel);
-        spinner.setVisibility(View.VISIBLE);
+        tvAbout = (TextView) findViewById(R.id.text_view_about);
 
         runOnUiThread(() -> {
-            webView = (WebView) findViewById(R.id.webView);
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                }
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    spinner.setVisibility(View.GONE);
-                }
-            });
-            webView.loadUrl(azinecUrl);
+            tvAbout.setText(getResources().getString(R.string.about_large_text));
         });
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setTitle("Azinec LLC");
+
+//        spinner = findViewById(R.id.loadingPanel);
+//        spinner.setVisibility(View.VISIBLE);
+
+//        runOnUiThread(() -> {
+//            webView = (WebView) findViewById(R.id.webView);
+//            webView.getSettings().setJavaScriptEnabled(true);
+//            webView.setWebViewClient(new WebViewClient() {
+//                @Override
+//                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+//                }
+//                @Override
+//                public void onPageFinished(WebView view, String url) {
+//                    spinner.setVisibility(View.GONE);
+//                }
+//            });
+//            webView.loadUrl(azinecUrl);
+//        });
 
     }
 
@@ -58,5 +68,6 @@ public class AboutActivity extends AppCompatActivity {
         Runtime.getRuntime().runFinalization();
         Runtime.getRuntime().gc();
     }
+
 
 }
